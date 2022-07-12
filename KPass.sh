6d0c75eaf6bcb4d7dff4c2ea34b2325eb4385f46
JCoupalK <108779415+JCoupalK@users.noreply.github.com>
2022-07-12 11:45:59 -0400

    Update KPass.sh

--- KPass.sh
+++ KPass.sh
@@ -19,16 +19,14 @@ printf "${NC}\n\n"
 printf "${GRN}This script must be run with 'autoexpect' (see https://github.com/KeepSec-Technologies/KPass)${NC}\n\n"
 sleep 0.5
 
-sudo mkdir /root/.kpass &> /dev/null
-cd /root/.kpass &> /dev/null
+sudo mkdir /root/.kpass &>/dev/null
+cd /root/.kpass &>/dev/null
 
 function installing {
   tput civis
   spinner="⣾⣽⣻⢿⡿⣟⣯⣷"
-  while :
-  do
-    for i in `seq 0 7`
-    do
+  while :; do
+    for i in $(seq 0 7); do
       printf "${PRPL}${spinner:$i:1}"
       printf "\010${NC}"
       sleep 0.2
@@ -41,23 +39,21 @@ SPIN_PID=$!
 disown
 printf "${PRPL}\nInstalling utilities ➜ ${NC}"
 
-if [ -n "`command -v apt-get`" ];
+if [ -n "$(command -v apt-get)" ]; then
+  sudo apt-get -y install perl >/dev/null && curl https://sh.rustup.rs -sSf &>/dev/null | sh -s -- -y &>/dev/null && sudo apt-get -y install cargo &>/dev/null
 
-then sudo apt-get -y install perl > /dev/null && curl https://sh.rustup.rs -sSf &> /dev/null | sh -s -- -y &> /dev/null && sudo apt-get -y install cargo &> /dev/null;
+elif [ -n "$(command -v yum)" ]; then
+  sudo yum -y install perl >/dev/null && curl https://sh.rustup.rs -sSf &>/dev/null | sh -s -- -y &>/dev/null && sudo yum -y install cargo &>/dev/null
 
-elif [ -n "`command -v yum`" ]; 
+else
 
-then sudo yum -y install perl > /dev/null && curl https://sh.rustup.rs -sSf &> /dev/null | sh -s -- -y &> /dev/null && sudo yum -y install cargo &> /dev/null;
+  printf "\n${RED}Your Linux distro is not supported, only RHEL-based and Debian-based is supported at the moment${NC}\n"
 
-else 
-
-printf "\n${RED}Your Linux distro is not supported, only RHEL-based and Debian-based is supported at the moment${NC}\n"
-
-exit
+  exit
 
 fi
 
-kill -9 $SPIN_PID &> /dev/null
+kill -9 $SPIN_PID &>/dev/null
 tput cnorm
 echo ""
 echo ""
@@ -65,21 +61,20 @@ echo ""
 
 #user confirmation
 function confirmUser {
-  
-read -p "What is the ${YEL}user${NC} that you want to use that script for : " User1
-echo ""
-grep1=$(grep -c $User1: /etc/passwd)
-userGrep="$(echo ${grep1})"
 
-if [[ $userGrep == 0 ]]; 
+  read -p "What is the ${YEL}user${NC} that you want to use that script for : " User1
+  echo ""
+  grep1=$(grep -c $User1: /etc/passwd)
+  userGrep="$(echo ${grep1})"
 
-then printf "${RED}This user does not exist! Try again\n\n${NC}";
+  if [[ $userGrep == 0 ]]; then
+    printf "${RED}This user does not exist! Try again\n\n${NC}"
 
-sleep 0.5; 
+    sleep 0.5
 
-confirmUser;
+    confirmUser
 
-fi
+  fi
 
 }
 
@@ -90,142 +85,135 @@ sleep 1
 
 #password confirmation
 function confirmPasswd1 {
-  
-read -s -p "What is the password that you want for ${YEL}Monday${NC} : " passwd1
-echo ""
-echo ""
-read -s -p "Confirm your password : " truepasswd1
-echo ""
-echo ""
-if [[ $passwd1 != $truepasswd1 ]]; 
 
-then printf "${RED}Passwords are different! Try again\n\n${NC}";
+  read -s -p "What is the password that you want for ${YEL}Monday${NC} : " passwd1
+  echo ""
+  echo ""
+  read -s -p "Confirm your password : " truepasswd1
+  echo ""
+  echo ""
+  if [[ $passwd1 != $truepasswd1 ]]; then
+    printf "${RED}Passwords are different! Try again\n\n${NC}"
 
-sleep 0.5; 
+    sleep 0.5
 
-confirmPasswd1;
+    confirmPasswd1
 
-fi
+  fi
 
 }
 
 function confirmPasswd2 {
-  
-read -s -p "What is the password that you want for ${YEL}Tuesday${NC} : " passwd2
-echo ""
-echo ""
-read -s -p "Confirm your password : " truepasswd2
-echo ""
-echo ""
-if [[ $passwd2 != $truepasswd2 ]]; 
 
-then printf "${RED}Passwords are different! Try again\n\n${NC}";
+  read -s -p "What is the password that you want for ${YEL}Tuesday${NC} : " passwd2
+  echo ""
+  echo ""
+  read -s -p "Confirm your password : " truepasswd2
+  echo ""
+  echo ""
+  if [[ $passwd2 != $truepasswd2 ]]; then
+    printf "${RED}Passwords are different! Try again\n\n${NC}"
 
-sleep 0.5; 
+    sleep 0.5
 
-confirmPasswd2;
+    confirmPasswd2
 
-fi
+  fi
 
 }
 
 function confirmPasswd3 {
-  
-read -s -p "What is the password that you want for ${YEL}Wednesday${NC} : " passwd3
-echo ""
-echo ""
-read -s -p "Confirm your password : " truepasswd3
-echo ""
-echo ""
-if [[ $passwd3 != $truepasswd3 ]]; 
 
-then printf "${RED}Passwords are different! Try again\n\n${NC}";
+  read -s -p "What is the password that you want for ${YEL}Wednesday${NC} : " passwd3
+  echo ""
+  echo ""
+  read -s -p "Confirm your password : " truepasswd3
+  echo ""
+  echo ""
+  if [[ $passwd3 != $truepasswd3 ]]; then
+    printf "${RED}Passwords are different! Try again\n\n${NC}"
 
-sleep 0.5; 
+    sleep 0.5
 
-confirmPasswd3;
+    confirmPasswd3
 
-fi
+  fi
 
 }
 
 function confirmPasswd4 {
-  
-read -s -p "What is the password that you want for ${YEL}Thursday${NC} : " passwd4
-echo ""
-echo ""
-read -s -p "Confirm your password : " truepasswd4
-echo ""
-echo ""
-if [[ $passwd4 != $truepasswd4 ]]; 
 
-then printf "${RED}Passwords are different! Try again\n\n${NC}";
+  read -s -p "What is the password that you want for ${YEL}Thursday${NC} : " passwd4
+  echo ""
+  echo ""
+  read -s -p "Confirm your password : " truepasswd4
+  echo ""
+  echo ""
+  if [[ $passwd4 != $truepasswd4 ]]; then
+    printf "${RED}Passwords are different! Try again\n\n${NC}"
 
-sleep 0.5; 
+    sleep 0.5
 
-confirmPasswd4;
+    confirmPasswd4
 
-fi
+  fi
 
 }
 
 function confirmPasswd5 {
-  
-read -s -p "What is the password that you want for ${YEL}Friday${NC} : " passwd5
-echo ""
-echo ""
-read -s -p "Confirm your password : " truepasswd5
-echo ""
-echo ""
-if [[ $passwd5 != $truepasswd5 ]]; 
 
-then printf "${RED}Passwords are different! Try again\n\n${NC}";
+  read -s -p "What is the password that you want for ${YEL}Friday${NC} : " passwd5
+  echo ""
+  echo ""
+  read -s -p "Confirm your password : " truepasswd5
+  echo ""
+  echo ""
+  if [[ $passwd5 != $truepasswd5 ]]; then
+    printf "${RED}Passwords are different! Try again\n\n${NC}"
 
-sleep 0.5; 
+    sleep 0.5
 
-confirmPasswd5;
+    confirmPasswd5
 
-fi
+  fi
 
 }
 
 function confirmPasswd6 {
-  
-read -s -p "What is the password that you want for ${YEL}Saturday${NC} : " passwd6
-echo ""
-echo ""
-read -s -p "Confirm your password : " truepasswd6
-echo ""
-echo ""
-if [[ $passwd6 != $truepasswd6 ]]; 
 
-then printf "${RED}Passwords are different! Try again\n\n${NC}";
+  read -s -p "What is the password that you want for ${YEL}Saturday${NC} : " passwd6
+  echo ""
+  echo ""
+  read -s -p "Confirm your password : " truepasswd6
+  echo ""
+  echo ""
+  if [[ $passwd6 != $truepasswd6 ]]; then
+    printf "${RED}Passwords are different! Try again\n\n${NC}"
 
-sleep 0.5; 
+    sleep 0.5
 
-confirmPasswd6;
+    confirmPasswd6
 
-fi
+  fi
 
 }
 
 function confirmPasswd7 {
-  
-read -s -p "What is the password that you want for ${YEL}Sunday${NC} : " passwd7
-echo ""
-echo ""
-read -s -p "Confirm your password : " truepasswd7
-echo ""
-echo ""
-if [[ $passwd7 != $truepasswd7 ]]; 
 
-then printf "${RED}Passwords are different! Try again\n\n${NC}";
+  read -s -p "What is the password that you want for ${YEL}Sunday${NC} : " passwd7
+  echo ""
+  echo ""
+  read -s -p "Confirm your password : " truepasswd7
+  echo ""
+  echo ""
+  if [[ $passwd7 != $truepasswd7 ]]; then
+    printf "${RED}Passwords are different! Try again\n\n${NC}"
 
-sleep 0.5; 
+    sleep 0.5
 
-confirmPasswd7;
+    confirmPasswd7
 
-fi
+  fi
 
 }
 
@@ -262,7 +250,6 @@ hash5=$(perl -e 'print crypt($ARGV[1], "\$" . $ARGV[0] . "\$" . $ARGV[2]), "\n";
 hash6=$(perl -e 'print crypt($ARGV[1], "\$" . $ARGV[0] . "\$" . $ARGV[2]), "\n";' "6" "$passwd6" "$salt6")
 hash7=$(perl -e 'print crypt($ARGV[1], "\$" . $ARGV[0] . "\$" . $ARGV[2]), "\n";' "6" "$passwd7" "$salt7")
 
-
 #password to set for each day
 Monday1=$(echo ${hash1})
 Tuesday1=$(echo ${hash2})
@@ -292,16 +279,17 @@ elif [ $whichday == "Sunday" ]; then
 fi
 
 #makes cronjob
-sudo chmod +x script.exp &> /dev/null
+sudo chmod +x script.exp &>/dev/null
 
-croncmd="(cd /root/.kpass/$User1 && ./script)>kpass.log"
+croncmd="(/usr/bin/expect /root/.kpass/$User1/script.exp)>/root/.kpass/$User1/kpass.log"
 cronjob="* 12 * * * $croncmd"
 
-echo "$cronjob" > /etc/cron.d/$User1-kpass
+printf "$cronjob\n" > /etc/cron.d/$User1-kpass
 printf "${GRN}\nWe're done!\n"
 echo ""
-printf "Please run this after exit:\n\n${YEL}(/root/.cargo/bin/rshc -f script.exp -o script.rs && rm -f script.rs script.exp.rs script.exp && mv script /root/.kpass/$User1) &> /dev/null\n\n${NC}"
+printf "Please run this after exit: ${YEL}mv script.exp /root/.kpass/$User1\n\n${NC}"
 
-sudo mkdir /root/.kpass/$User1 &> /dev/null
+sudo mkdir /root/.kpass/$User1 &>/dev/null
+(chmod 700 /root/.kpass && chmod 700 /root/.kpass/$User1 && chmod 700 /root/.kpass/KPass.sh && chmod 700 /etc/cron.d/$User1-kpass && chmod 700 /root/.kpass/$User1/script.exp) &>/dev/null
 
 exit
