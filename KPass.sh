c7c3520395d76312a5a3ecabb10dca1f4dc0587a
JCoupalK <108779415+JCoupalK@users.noreply.github.com>
2022-07-14 15:22:58 -0400

    Final commit

--- KPass.sh
+++ KPass.sh
@@ -18,6 +18,7 @@ printf "${NC}\n\n"
 
 printf "${GRN}This script must be run with 'autoexpect' (see https://github.com/KeepSec-Technologies/KPass)${NC}\n\n"
 
+chmod 700 $PWD/KPass.sh
 sudo mkdir /root/.kpass &>/dev/null
 cd /root/.kpass &> /dev/null
 
@@ -291,5 +292,5 @@ printf "Please run this after exit:\n\n${YEL}(/root/.cargo/bin/rshc -f script.ex
 
 sudo mkdir /root/.kpass/$User1 &>/dev/null
 
-(chmod 700 /root/.kpass && chmod 700 /root/.kpass/$User1 && chmod 700 /root/.kpass/$User1/kpass.log && chmod 700 $PWD/KPass.sh && chmod 700 /etc/cron.d/$User1-kpass && chmod 700 /root/.kpass/$User1/script) &>/dev/null
+(chmod 700 /root/.kpass && chmod 700 /root/.kpass/$User1 && chmod 700 /root/.kpass/$User1/kpass.log && chmod 700 /etc/cron.d/$User1-kpass && chmod 700 /root/.kpass/$User1/script) &>/dev/null
 exit
